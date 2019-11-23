f0b09e5287ea1720707258e3ba5833f04f54d74a
MTH <coo500559@gmail.com>
2019-11-23 17:35:27 +0900

    DDDD

--- src/Subject.java
+++ src/Subject.java
@@ -2,52 +2,325 @@ import java.util.Scanner;
 
 public class Subject {
 
-		
-	
-	private static final Object A3 = null;
-
 	public static void main(String[] args) {
-		
+
 		Scanner s = new Scanner(System.in);
-		
 
-		System.out.println("심리테스트 입니다. yes 나 no 로 대답해 주세요");
+		System.out.println("<심리테스트 입니다. yes 나 no 로 대답해 주세요!>");
 		System.out.println("나는 금사빠다 (금방 사랑에 빠진다)");
-	
+		System.out.println(" ");
 		System.out.println("<yes or no>");
-		String A1 = s.nextLine();
-		
-		String a = "나는 금사빠다";
-		String b = "연예할때 끌려다니는 타입이다";
-		String c = "데이터 코스는 미리 짜는게 편하다";
-		String d = "활동적인 데이트가 좋다";
-		String e = "감정 표현에 솔직한 편이다";
-		String f = "감정 기복이 크지 않다";
-		String g = "연락이 없어도 믿고 기다리는 편이다";
-		String h = "이성친구는 존재 할 수 없다";
-		String i = "아무것도 아닌 일에 쉽게 섭섭함이 쌓인다";
-		String j = "당신의 유형은 A 입니다";
-		String k = "당신의 유형은 B 입니다";
-		String l= "당신의 유형은 C 입니다";
-		String m = "당신의 유형은 D 입니다";
-		
-		
-		if (A1.equalsIgnoreCase ("yes")){
-			System.out.println("연애할때 끌려다니는 타입이다");
-			System.out.println("<yes or no>");
-			String A2 = s.nextLine();
-		}else{
-			System.out.println("감정의 기복이 크지 않다");
-			System.out.println("<yes or no>");
-			String A2 = s.nextLine();
-		if	(A1.equalsIgnoreCase ("yes")){
-			System.out.println("연락이 없어도 믿고 기다리는 편이다");
-			System.out.println("<yes or no>");
-			String A3 = s.nextLine();
-		}	
-		
-					}
-		 				
-	   }
+		String a = s.nextLine();
 
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("연예할때 끌려다니는 타입이다");
+			a = s.nextLine();
+
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("감정 표현에 솔직한 편이다");
+			a = s.nextLine();
+
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("이성친구는 존재할 수없다");
+			a = s.nextLine();
+
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌 일에 쉽게 섭섭함이 쌓인다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("당신의 유형은 ' D ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' C ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' B ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("활동적인 데이트가 좋다");
+			a = s.nextLine();
+
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("이성친구는 존재할 수 없다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌 일에 쉽게 섭섭함이 쌓인다");
+			a = s.nextLine();
+
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("당신의 유형은 ' D ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' C ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 'B ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌 일에 쉽게 섭섭함이 쌓인다");
+			a = s.nextLine();
+
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("당신의 유형은 D 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' C ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("데이트 코스는 미리 짜는게 편하다");
+			a = s.nextLine();
+
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("활동적인 데이트가 좋다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("이성친구는 존재할 수 없다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌일에 쉽게 섭섭함이 쌓인다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("당신의 유형은 ' D ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' C ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' B ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌 일에 쉽게 섭섭함이 쌓인다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("당신의 유형은 ' D ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' C ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("감정표현에 솔직한 편이다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("이성친구는 존재할 수 없다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌 일에 쉽게 섭섭함이 쌓인다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' B ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("활동적인 데이트가 좋다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("이성친구는 존재할 수 없다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌 일에 쉽게 섭섭함이 쌓인다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' B ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌 일에 쉽게 섭섭함이 쌓인다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("당신의 유형은 ' D ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' C ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("감정기복이 크지 않다");
+			a = s.nextLine();
+
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("연락이 없어도 믿고 기다리는 편이다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("당신의 유형은 ' A ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("이성친구는 존재할 수 없다.");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌 일에 쉽게 섭섭함이 쌓인다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("당신의 유형은 ' D ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' C ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' B ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("감정 표현에 솔직한 편이다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("이성친구는 존재할 수 없다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌 일에 쉽게 섭섭함이 쌓인다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("당신의 유형은 ' D ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' C ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' B ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("활동적인 데이트가 좋다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("이성친구는 존재할 수 없다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌 일게 쉽게 섭섭함이 샇인다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("당신의 유형은 ' D ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' C ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' B ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("(yes 나 no 로 대답해 주세요!)");
+			System.out.println("아무것도 아닌 일에 쉽게 섭섭함이 쌓인다");
+			a = s.nextLine();
+		}
+		if (a.equalsIgnoreCase("yes")) {
+			System.out.println("당신의 유형은 ' D ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		} else if (a.equalsIgnoreCase("no")) {
+			System.out.println("당신의 유형은 ' C ' 입니다");
+			System.out.println("이상으로 테스트를 마칩니다");
+			a = s.nextLine();
+
+		}
+
+	}
 }
---
+++ src/subject1.java
@@ -0,0 +1,9 @@
+}if ( a .equalsIgnoreCase ("yes")){
+System.out.println("(yes 나 no 로 대답해 주세요!)");
+System.out.println("감정 표현에 솔직한 편이다");
+a = s.nextLine();
+					 
+}else if ( a .equalsIgnoreCase ("no")){
+System.out.println("(yes 나 no 로 대답해 주세요!)");
+System.out.println("데이트 코스는 미리 짜는게 편하다"); 
+a = s.nextLine();
