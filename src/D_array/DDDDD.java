3e351c3c1bd577a4e27e4b476eb965e362c8c664
MTH <coo500559@gmail.com>
2019-11-27 21:25:29 +0900

    dd

--- src/D_array/DDDDD.java
+++ src/D_array/DDDDD.java
@@ -5,29 +5,23 @@ import java.util.Arrays;
 public class DDDDD {
 
 	public static void main(String[] args) {
-		// 배열에 저장된 숫자들 중 최소값과 최대값을 찾아주세요.
-		
-		
-		
-		int []numbers = new int [10];
-		
-		for (int i = 0; i < numbers.length; i++) {
-			numbers[i] = (int) (Math.random() * 100) + 1;
-		}
-		int min = numbers[0];
-		int max = numbers[0];
-		
-		int [] shuffle = new int [30];
-		for (int i = 0; i < shuffle.length; i++) {
-			shuffle[i] = i + 1;
-		}
-		for (int i = 0; i < shuffle.length *100; i++) {
-			int random = (int)(Math.random() * shuffle.length);
-			int temp = shuffle[0];
-			shuffle[0] = shuffle[random];
-			shuffle[random] = temp;
+		//0번 인덱스의 값과 랜덤 인덱스의 값을 서로 교환한다
+		int [] shuffle = new int [20];
+		for(int i = 0; i < shuffle.length; i++ ){
 			
-			}System.out.println(Arrays.toString(shuffle));
+				shuffle[i] += i +1 ;
+		}		
+		for(int i = 0; i < shuffle.length; i++ ){
+			
+				int random = (int)(Math.random()*20) + 1;				
+				
+				int temp  =  shuffle[0];
+				shuffle [0] = shuffle[random];
+				shuffle [random] = temp;
+			
+		}
+		
+		
 		}
 	}
 
