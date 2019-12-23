8f5191b34d8258d8ff40135848c4fcc898ab249f
MTH <coo500559@gmail.com>
2019-12-23 12:49:35 +0900

    2019-12-23

--- src/Basdas.java
+++ src/Basdas.java
@@ -3,20 +3,21 @@ import java.util.*;
 public class Basdas {
 	public static void main(String[] args) {
 		
-	
-		   int answer = 0;
-		      String [] s = new String []{"1","2","3","4","5"};
-		      
-		      for(int i = 0; i < s.length; i++){
-		          System.out.print(toString(s));
-		      }
-
-		   
-
-	}
-
-	private static char[] toString(String[] s) {
-		// TODO Auto-generated method stub
-		return null;
+/*		Scanner s = new Scanner(System.in);
+		System.out.println("제목을 입력해 주세요");
+		String a = s.nextLine();
+		System.out.println("내용을 입력해 주세요");
+		String b = s.nextLine();
+		
+		System.out.println("제목 :" + a);
+		System.out.println("내용 :" + b);*/
+		int a1 = 0;
+		for (int i = 0; i <= 10; i++){
+			
+				a1 = a1+i;
+			 
+		}
+		System.out.println(a1);
+		
 	}
 }
